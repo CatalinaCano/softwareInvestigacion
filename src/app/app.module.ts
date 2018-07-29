7ecb932fc7b3f5819c3d0d973090df4eb57b5d59
Catalina Cano <catalinacano@MacBook-Pro-de-Catalina.local>
2018-07-29 17:51:22 -0500

    Creacion de los componentes de las paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
